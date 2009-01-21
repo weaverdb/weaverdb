348e952e1fa7f39e2a72a1b125f26c92121ba242
Myron Scott <mkscott@sacadia.com>
2009-01-20 22:33:50 -0800

    use per instance temp dir and pass name into java

--- mtpgsql/src/include/env/timestamp.h
+++ mtpgsql/src/include/env/timestamp.h
@@ -2,7 +2,7 @@
 
 #ifndef BUILDTIME
 
-#define BUILDTIME "2009-01-14 11:04:02"
+#define BUILDTIME "2009-01-20 15:23:59"
 
 #endif
 
--- pgjava_c/main.c
+++ pgjava_c/main.c
@@ -9,6 +9,7 @@
 
 #include "main.h"
 
+static int cleanupTempDir(char* path);
 
 int main(int argc,char* argv[])
 {
@@ -18,16 +19,21 @@ int main(int argc,char* argv[])
         char* classes = getenv("CLASSPATH");
         char		pclass[4096];
         char		plibs[4096];
+        char		ptemp[256];
+        char            tmpdir[256];
+        
         int err = 0;
         int extras = argc - 2;
-/*
-        printf("extras = %d\n",extras);
-*/
+
         if ( extras < 0 ) extras = 0;
 
         if ( classes == NULL || strlen(classes) == 0 ) {
-            printf("CLASSPATH must be set");
-            exit(1);
+            classes = malloc(4096);
+            char* mtpg = getenv("MTPG");
+            if ( mtpg == NULL || strlen(classes) == 0 ) {
+                mtpg = "mtpg";
+            }
+            sprintf(classes,"%s/server/base_server.jar:%s/server/lib/basedata.jar:%s/lib/weaver.jar",mtpg,mtpg,mtpg);
         }
 
         /*  set up memory allocation mtpgjava does 8k blocks so lets use that
@@ -39,40 +45,49 @@ int main(int argc,char* argv[])
 */
 #endif	
 
-      	 if (strstr(argv[0],"weaver_server") != NULL ) {
-			pid_t  w = fork();
-			if ( w == 0 ) {
-				setsid();
-			} else {
-				printf("database process id: %d\n",w);
-				exit(0);
-			}
-		} 
+      	if (strstr(argv[0],"weaver_server") != NULL ) {
+            setsid();
 
-      JavaVMInitArgs vm_args;
+            pid_t  w = fork();
+            if ( w == 0 ) {
+                setsid();
+            } else {
+                printf("database process id: %d\n",w);
+                exit(0);
+            }
+        }
+        
+        sprintf(tmpdir,"/var/tmp/weaver-%d",getpid());
+
+       JavaVMInitArgs vm_args;
        JavaVMOption options[15];
 
        memset(pclass,0,sizeof(pclass));
        memset(plibs,0,sizeof(plibs));
+       memset(ptemp,0,sizeof(ptemp));
        
        sprintf(pclass,"-Djava.class.path=%s",classes);
        sprintf(plibs,"-Djava.library.path=%s",getenv("LD_LIBRARY_PATH"));
+       sprintf(ptemp,"-Djava.io.tmpdir=%s",tmpdir);
 
        options[0].optionString = pclass; /* user classes */
        options[0].extraInfo = NULL;                  
        options[1].optionString = plibs;  /* set native library path */
        options[1].extraInfo = NULL;    
-                     
+       options[2].optionString = ptemp;  /* set tempdir path */
+       options[2].extraInfo = NULL;
+
 	for (;extras > 0;extras--) {
-	        options[1+extras].optionString = argv[extras];
-                options[1+extras].extraInfo = NULL;    
+	        options[2+extras].optionString = argv[extras];
+                options[2+extras].extraInfo = NULL;
 	}
 
        vm_args.version = JNI_VERSION_1_2;
        vm_args.options = options;
-       vm_args.nOptions = argc;   /*  argc minus the program name "pgjava" and the properties file + the extras and the 
-                                    *    classpath librarypath added here 
-                                    */
+       vm_args.nOptions = 3 + argc - 2;
+       /*  extras +
+        *      ( classpath + libpath + temppath )
+        */
        vm_args.ignoreUnrecognized = JNI_FALSE;
        
         /* load and initialize a Java VM, return a JNI interface 
@@ -93,11 +108,30 @@ int main(int argc,char* argv[])
 */
         jobject pop = (*env)->NewStringUTF(env,argv[argc-1]);
         jobjectArray job = (*env)->NewObjectArray(env,1,string,pop);
+
+        mkdir(tmpdir,S_IRWXU);
+
         (*env)->CallStaticVoidMethod(env,cls, mid, job);
+
+        int test = cleanupTempDir(tmpdir);
         
         (*jvm)->DetachCurrentThread(jvm);
         /* We are done. */
-
        (*jvm)->DestroyJavaVM(jvm);
+}
+
+static int cleanupTempDir(char* path) {
+	FILE *p;
+        char msg[512];
+        sprintf(msg,"/usr/bin/rm -r %s",path);
+	if ((p = popen(msg, "r")) == NULL ) {
+            perror("SYSTEM FAILURE");
+		return (-1);
+        }
+
+        while (fgets(msg, 512, p) != NULL)
+             printf("%s", msg);
+
+        return (pclose(p));
 }
 
